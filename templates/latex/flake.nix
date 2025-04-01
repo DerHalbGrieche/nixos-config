0edf02b3f20f215c126ec2cfddef4be5a4502003
Vasilis Manetas <vasilis12.manetas@gmail.com>
2025-04-01 11:31:50 +0200

    add build to latex flake

--- templates/latex/flake.nix
+++ templates/latex/flake.nix
@@ -5,13 +5,32 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: {
-    devShells.x86_64-linux = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [texlab texliveFull];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    # This is the default package that builds the PDF.
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      pname = "latex-document";
+      version = "1.0";
+      src = ./.;
+      buildInputs = [pkgs.texliveFull];
+      buildPhase = ''
+        pdflatex main.tex
+        pdflatex main.tex
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp main.pdf $out/
+      '';
+    };
+
+    # Dev shell for editing with tools like texlab.
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [texlab texliveFull];
     };
   };
 }
