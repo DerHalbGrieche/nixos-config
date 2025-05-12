5f4c27aab0edb0f3f30a662425994a013381512f
Vasilis Manetas <vasilis12.manetas@gmail.com>
2025-05-12 14:08:54 +0200

    add custom search engines

--- home-manager/vasilis/gui/firefox.nix
+++ home-manager/vasilis/gui/firefox.nix
@@ -15,14 +15,64 @@
           vimium
         ];
       };
+      search = {
+        default = "ddg";
+        engines = {
+          nix-packages = {
+            name = "Nix Packages";
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@np"];
+          };
+
+          nixos-wiki = {
+            name = "NixOS Wiki";
+            urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
+            iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+            definedAliases = ["@nw"];
+          };
+          nueschtos = {
+            name = "Nüschtos";
+            urls = [{template = "https://search.nüschtos.de/?query={searchTerms}";}];
+            iconMapObj."16" = "https://search.nüschtos.de/favicon.ico";
+            definedAliases = ["@ns"];
+          };
+
+          bing.metaData.hidden = true;
+          google.metaData.alias = "@g";
+        };
+      };
     };
   };
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = "firefox.desktop";
-    "text/xml" = "firefox.desktop";
-    "application/xhtml+xml" = "firefox.desktop";
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
-  };
+  xdg.mimeApps.defaultApplications = let
+    mimeTypes = [
+      "text/html"
+      "text/xml"
+      "application/xhtml+xml"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "application/pdf"
+    ];
+  in
+    builtins.listToAttrs (map (mime: {
+        name = mime;
+        value = "firefox.desktop";
+      })
+      mimeTypes);
   home.sessionVariables.DEFAULT_BROWSER = "firefox";
 }
